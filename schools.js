fc5dd3854f43fbf903bf4df748bbe5fbe388e8f1
zevrost471 <zebmascensao00@gmail.com>
2025-11-30 00:02:43 +0000

    add classes.js and schools.js

---
+++ schools.js
@@ -0,0 +1,9 @@
+export const schools = Object.freeze({
+    ARCANE: "Arcane",
+    FIRE: "Fire",
+    FROST: "Frost",
+    HOLY: "Holy",
+    NATURE: "Nature",
+    PHYSICAL: "Physical",
+    SHADOW: "Shadow"
+});
